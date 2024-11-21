f480783c1d7196f4899bc37e936a29c7f5403ddc
Julian Hernandez <157622466+JulianHL@users.noreply.github.com>
2024-11-21 04:59:12 -0500

    StudentBook Service Added

---
+++ iLib/Services/StudentBookService.cs
@@ -0,0 +1,31 @@
+﻿using iLib.Models;
+using iLib.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace iLib.Services
+{
+    public class StudentBookService : BaseService
+    {
+
+        public List<StudentBooks> GetAllStudentBooksByStudentId()
+        {
+            using (SqlConnection? connection = EstablishConnection())
+            {
+                if (connection == null)
+                {
+                    throw new Exception("The connection was not established, connection is null");
+                }
+
+                DBStudentBooksTable dB = new DBStudentBooksTable();
+                List<StudentBooks> studentBooks = new List<StudentBooks>();
+                if (studentBooks == null)
+                {
+                    throw new Exception("The connection was not established, connection is null");
+                }
+
+                return studentBooks;
+            }
+        }
+
+    }
+}
