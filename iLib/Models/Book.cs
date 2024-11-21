99bcc01003eb45c6af9c7e0b722c6516fd54fe97
Julian Hernandez <157622466+JulianHL@users.noreply.github.com>
2024-11-21 04:24:05 -0500

    Book Model Added

---
+++ iLib/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace iLib.Models
+{
+    public class Book
+    {
+        public String BookIsbn { get; set; }
+        public String BookTitle { get; set; }
+        public String BookAuthor {  get; set; }
+        public int BookQuantity { get; set; }
+        public String BookPublisher { get; set; }
+        public String BookGenre { get; set; }
+        public String BookLanguage { get; set; }
+        public String BookFormat { get; set; }
+        public String? BookDescription { get; set; }
+        public String? BookEdition { get; set; }
+        public int? BookPages { get; set; }
+        public DateOnly? BookPublicationDate { get; set; }
+
+    }
+}
