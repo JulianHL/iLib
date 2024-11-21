74a26ffdaafd74de8baa25b21ff02d35f680345e
Julian Hernandez <157622466+JulianHL@users.noreply.github.com>
2024-11-21 04:29:12 -0500

    StudentBooks Model Added

---
+++ iLib/Models/StudentBooks.cs
@@ -0,0 +1,8 @@
+﻿namespace iLib.Models
+{
+    public class BorrowedBook : Book
+    {
+        public DateOnly BookStartingDate { get; set; }
+        public DateOnly BookDueDate { get; set; }
+    }
+}
