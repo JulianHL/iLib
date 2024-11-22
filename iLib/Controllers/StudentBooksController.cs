03000e6db35762620abd58b4b12716eb9c9e28ee
Julian Hernandez <157622466+JulianHL@users.noreply.github.com>
2024-11-22 13:39:09 -0500

    Implementation Updated

--- iLib/Controllers/StudentBooksController.cs
+++ iLib/Controllers/StudentBooksController.cs
@@ -13,7 +13,6 @@ namespace iLib.Controllers
             {
                 _logger = logger;
             }
-            // Test Git JIRA
             public IActionResult Index()
             {
             try
