493c75308daab7d2b6c04ef5b8915c197ed886e5
Julian Hernandez <157622466+JulianHL@users.noreply.github.com>
2024-11-26 14:53:06 -0500

    iLib deleted

--- iLib/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace iLib.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
